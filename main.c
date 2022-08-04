429766f382698c01fcae06495197282b18ab910e
Mario Merlo <18537290+MrVideo@users.noreply.github.com>
2022-08-04 15:46:38 +0200

    More "progress"

--- main.c
+++ main.c
@@ -2,13 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 
-// This is the start of something awful
+#define BUFSIZE 1024
+
+// This is the start of something awful.
 
 // Global variables
 int word_length, tries;
-char buffer[1024];      // This is the string buffer to check for commands passed from the user
-int command_chk = 0;    // This value checks whether a command was passed to the program and breaks different loops
-char password[256];     // This is the word that the player has to guess
+char buffer[BUFSIZE];   // This is the string buffer to check for commands and guesses passed by the user.
+int command_chk = 0;    // This value checks whether a command was passed to the program and breaks different loops.
+char password[256];     // This is the word that the player has to guess.
+int win = 0;            // This variable lets the game end when the player guesses the word.
 
 // Node
 struct node {
@@ -26,6 +29,13 @@ list wordlist = NULL;
 // List functions
 list list_add(list l, char *string);
 list search(list l, char *string);
+
+// Support functions
+void empty_buffer(char *b);
+
+// Game functions
+void filtered_print(list l);
+void add_new(list l);
 
 int main() {
     // Initialisation
@@ -50,9 +60,24 @@ int main() {
     // Game start
     printf("Inizio nuova partita\n");
     printf("Inserisci la parola da indovinare: ");
+    // Saves the password to guess
     if(scanf("%s", password)) {
-        if(search(wordlist, password) == NULL)
-            printf("La parola da indovinare dev'essere inclusa tra quelle ammissibili.\n");
+        // Asks user for tries
+        printf("\nInserisci il numero dei tentativi: ");
+        if(scanf("%d", &tries)) {
+            // The player starts guessing
+            while(tries && !win) {
+                empty_buffer(buffer);
+                if(scanf("%s", buffer)) {
+                    if(!strcmp(buffer, password))
+                        win = 1;
+                    else if(!strcmp(buffer, "+stampa_filtrate"))
+                        filtered_print(wordlist);
+                    else if(!strcmp(buffer, "+inserisci_inizio"))
+                        add_new(wordlist);
+                }
+            }
+        }
     }
 }
 
@@ -75,4 +100,17 @@ list search(list l, char *string) {
         return l;
     else
         return search(l -> next, string);
+}
+
+void empty_buffer(char *b) {
+    for (int i = 0; i < BUFSIZE; ++i)
+        b[i] = '\0';
+}
+
+void filtered_print(list l) {
+
+}
+
+void add_new(list l) {
+
 }
