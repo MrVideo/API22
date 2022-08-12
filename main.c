84db3e75106aa0a4176fd5e57787bd6ed010b690
Mario Merlo <18537290+MrVideo@users.noreply.github.com>
2022-08-12 12:10:31 +0200

    Major refactoring and changes.
    - Program does not terminate once a player has won or lost.
    - Words are now checked in a different function than main.
    - New struct to check game status.

--- main.c
+++ main.c
@@ -9,6 +9,11 @@ struct node {
     struct node *next;
 };
 
+struct check {
+    int ok;
+    int tries;
+};
+
 typedef struct node *list;
 
 list add_sort(list l, char *new_data);
@@ -16,14 +21,24 @@ void print(list l);
 list delete(list l, char *to_delete);
 list search(list l, char *query);
 void empty_buffer(char *b);
+list add_new_words(list l, int len);
+struct check word_check(char *password, char *buffer, char *guide, int word_length, struct check status);
 
 int main() {
     // Variable declarations
     char *buffer = calloc(BUFSIZE, sizeof(char));
     char *password = calloc(BUFSIZE, sizeof(char));
     char *guide = calloc(BUFSIZE, sizeof(char));
-    int word_length, cmd_chk = 0, tries, ok = 0;
+    int word_length, cmd_chk = 0;
+    struct check status;
     list wordlist = NULL;
+
+    // Check struct initialisation
+    status.ok = 0;
+    status.tries = 0;
+
+    // -- Game setup --
+    // This is only run once.
 
     // Saves the word length decided by the user.
     word_length = (int)strtol(fgets(buffer, BUFSIZE, stdin), NULL, 10);
@@ -46,86 +61,69 @@ int main() {
         if(search(wordlist, buffer) != NULL)
             password = strdup(buffer);
     }
+
     // Asks the user for the number of tries in the game.
-    if(scanf("%d", &tries) != 0) {
-        do {
-            empty_buffer(buffer);
-            if(scanf("%s", buffer) != 0) {
-                if(strlen(buffer) != word_length)
+    if(scanf("%d", &status.tries) != 0) {
+
+        // -- Main game loop --
+        // This is run until EOF is read from stdin.
+
+        while(1) {
+            // Checks for EOF to terminate program
+            if(scanf("%s", buffer) == EOF)
+                return 0;
+            else {
+                // If the "+inserisci_inizio" command is passed, new words are added to the wordlist through
+                // the "add_new_words" function.
+                if(strcmp(buffer, "+inserisci_inizio") == 0) {
+                    wordlist = add_new_words(wordlist, word_length);
+                    empty_buffer(buffer);
+                    continue;
+                }
+                // If the word is not of the correct length or it does not appear in the wordlist,
+                // no tries are consumed and the user can retry.
+                else if(strlen(buffer) != word_length) {
                     printf("not_exists");
-                else {
-                    if(!strcmp(buffer, password))
-                        ok = 1;
-                    else {
-                        for(int i = 0; i < word_length; ++i)
-                            guide[i] = '\0';
-                        --tries;
-                        for(int i = 0; i < word_length; ++i) {
-                            if(password[i] == buffer[i])
-                                guide[i] = '+';
-                            else if(strchr(password, buffer[i]) == NULL)
-                                guide[i] = '/';
+                    empty_buffer(buffer);
+                    continue;
+                }
+                // If none of the checks before this get triggered, the word is checked for correctness.
+                else if(!status.ok && status.tries > 0) {
+                        status = word_check(password, buffer, guide, word_length, status);
+                        empty_buffer(buffer);
+                        if(!status.ok && status.tries >= 0) {
+                            for (int i = 0; i < word_length; ++i)
+                                printf("%c", guide[i]);
+                            printf("\n");
+                            if(status.tries != 0) continue;
                         }
-                        int pwd_count, ok_count, misplaced_count, k = 0, l;
-                        char tmp_chr;
-                        while(k < word_length) {
-                            pwd_count = 0;
-                            ok_count = 0;
-                            misplaced_count = 0;
-                            if(guide[k] != '+' && guide[k] != '/') {
-                                tmp_chr = buffer[k];
-                                for(int i = 0; i < word_length; ++i) {
-                                    if(buffer[i] == tmp_chr && password[i] != tmp_chr && guide[i] != '/')
-                                        ++misplaced_count;
-                                    if(buffer[i] == tmp_chr && guide[i] == '+')
-                                        ++ok_count;
-                                    if(password[i] == tmp_chr)
-                                        ++pwd_count;
-                                }
-                                l = 0;
-                                if(ok_count == pwd_count) {
-                                    while(misplaced_count > 0 && l < word_length) {
-                                        if(buffer[l] == tmp_chr && guide[l] != '+' && guide[l] != '/') {
-                                            guide[l] = '/';
-                                            --misplaced_count;
-                                        }
-                                        ++l;
-                                    }
-                                } else if(pwd_count > ok_count) {
-                                    while(misplaced_count > 0 && l < word_length) {
-                                        if(buffer[l] == tmp_chr && guide[l] != '+' && guide[l] != '/') {
-                                            if(pwd_count > ok_count) {
-                                                guide[l] = '|';
-                                                --misplaced_count;
-                                                --pwd_count;
-                                            } else if(pwd_count == ok_count) {
-                                                guide[l] = '/';
-                                                --misplaced_count;
-                                            }
-                                        }
-                                        ++l;
-                                    }
-                                }
+                }
+            }
+
+            // Prints the outcome and resets the game.
+            // TODO fix game restart
+            if(status.ok || status.tries == 0) {
+                if(status.ok) printf("ok\n");
+                if(!status.tries) printf("ko\n");
+                status.ok = 0;
+                empty_buffer(buffer);
+                if(scanf("%s", buffer) != 0) {
+                    if(strcmp(buffer, "+inserisci_inizio") == 0)
+                        wordlist = add_new_words(wordlist, word_length);
+                    if(strcmp(buffer, "+nuova_partita") == 0) {
+                        empty_buffer(buffer);
+                        if(scanf("%s", buffer) != 0)
+                            if(search(wordlist, buffer) != NULL) {
+                                empty_buffer(password);
+                                password = strdup(buffer);
                             }
-                            ++k;
-                        }
+                        if(scanf("%d", &status.tries) != 0)
+                            continue;
                     }
                 }
             }
-            if(!ok) {
-                for (int i = 0; i < word_length; ++i)
-                    printf("%c", guide[i]);
-                printf("\n");
-            }
-        } while(ok == 0 && tries > 0);
+        }
     }
-
-    // Prints the outcome of the game.
-    if(ok) printf("ok");
-    if(!tries) printf("ko");
-
-    // Terminates program.
-    return 0;
 }
 
 list add_sort(list l, char *new_data) {
@@ -209,4 +207,88 @@ list search(list l, char *query) {
 void empty_buffer(char *b) {
     for(int i = 0; i < BUFSIZE; ++i)
         b[i] = '\0';
+}
+
+list add_new_words(list l, int len) {
+    char *buffer = calloc(BUFSIZE, sizeof(char));
+    int exit = 0;
+
+    while(!exit) {
+        if(scanf("%s", buffer) != 0) {
+            if(strcmp(buffer, "+inserisci_fine") == 0)
+                exit = 1;
+            else if(strlen(buffer) == len)
+                l = add_sort(l, buffer);
+        }
+        empty_buffer(buffer);
+    }
+
+    return l;
+}
+
+struct check word_check(char *password, char *buffer, char *guide, int word_length, struct check status) {
+    // Correct word
+    if(!strcmp(buffer, password))
+        status.ok = 1;
+    else {
+        // Empty guide buffer
+        for(int i = 0; i < word_length; ++i)
+            guide[i] = '\0';
+        // Wrong word
+        --status.tries;
+        for(int i = 0; i < word_length; ++i) {
+            // Correct character check
+            if(password[i] == buffer[i])
+                guide[i] = '+';
+            // Check for character not in string
+            else if(strchr(password, buffer[i]) == NULL)
+                guide[i] = '/';
+        }
+        // Local count variables declaration
+        int pwd_count, ok_count, misplaced_count, k = 0, l;
+        char tmp_chr;
+        // Misplaced character check
+        while(k < word_length) {
+            pwd_count = 0;
+            ok_count = 0;
+            misplaced_count = 0;
+            if(guide[k] != '+' && guide[k] != '/') {
+                tmp_chr = buffer[k];
+                for(int i = 0; i < word_length; ++i) {
+                    if(buffer[i] == tmp_chr && password[i] != tmp_chr && guide[i] != '/')
+                        ++misplaced_count;
+                    if(buffer[i] == tmp_chr && guide[i] == '+')
+                        ++ok_count;
+                    if(password[i] == tmp_chr)
+                        ++pwd_count;
+                }
+                l = 0;
+                if(ok_count == pwd_count) {
+                    while(misplaced_count > 0 && l < word_length) {
+                        if(buffer[l] == tmp_chr && guide[l] != '+' && guide[l] != '/') {
+                            guide[l] = '/';
+                            --misplaced_count;
+                        }
+                        ++l;
+                    }
+                } else if(pwd_count > ok_count) {
+                    while(misplaced_count > 0 && l < word_length) {
+                        if(buffer[l] == tmp_chr && guide[l] != '+' && guide[l] != '/') {
+                            if(pwd_count > ok_count) {
+                                guide[l] = '|';
+                                --misplaced_count;
+                                --pwd_count;
+                            } else if(pwd_count == ok_count) {
+                                guide[l] = '/';
+                                --misplaced_count;
+                            }
+                        }
+                        ++l;
+                    }
+                }
+            }
+            ++k;
+        }
+    }
+    return status;
 }
