eaec9e4f1b7c0a006c079e9b79b2d1d7df316e8d
Mario <18537290+MrVideo@users.noreply.github.com>
2022-09-02 15:18:58 +0200

    Changed paths in runtest.sh to allow running on other machines.

--- runtest.sh
+++ runtest.sh
@@ -1,11 +1,11 @@
 #!/bin/zsh
 
 # Resets the output folder
-rm -rf $HOME/GitHub/FinalProject22/output
-mkdir $HOME/GitHub/FinalProject22/output
+rm -rf ~/GitHub/FinalProject22/output
+mkdir ~/GitHub/FinalProject22/output
 
 # Compiles the program as a release build
-gcc $HOME/GitHub/FinalProject22/main.c -O2 -Wall -Werror -o $HOME/GitHub/FinalProject22/build/FinalProject22 > /dev/null 2>&1
+gcc ~/GitHub/FinalProject22/main.c -O2 -Wall -Werror -o ~/GitHub/FinalProject22/cmake-build-debug/FinalProject22 > /dev/null 2>&1
 if [ $? -ne 0 ]; then
     echo "Compilation failed."
     exit 1
@@ -13,8 +13,8 @@ fi
 
 # Runs tests
 echo "Running test 'Insert'..."
-$HOME/GitHub/FinalProject22/build/FinalProject22 < $HOME/GitHub/FinalProject22/tests/insert.txt > $HOME/GitHub/FinalProject22/output/insertout.txt
-diff $HOME/GitHub/FinalProject22/output/insertout.txt $HOME/GitHub/FinalProject22/tests/insert.output.txt > /dev/null 2>&1
+~/GitHub/FinalProject22/cmake-build-debug/FinalProject22 < ~/GitHub/FinalProject22/tests/insert.txt > ~/GitHub/FinalProject22/output/insertout.txt
+diff ~/GitHub/FinalProject22/output/insertout.txt ~/GitHub/FinalProject22/tests/insert.output.txt > /dev/null 2>&1
 if [ $? -eq 0 ]; then
     echo "Test 'Insert' passed"
 else
@@ -22,8 +22,8 @@ else
 fi
 
 echo "Running test 'Slide'..."
-$HOME/GitHub/FinalProject22/build/FinalProject22 < $HOME/GitHub/FinalProject22/tests/slide.txt > $HOME/GitHub/FinalProject22/output/slideout.txt
-diff $HOME/GitHub/FinalProject22/output/slideout.txt $HOME/GitHub/FinalProject22/tests/slide.output.txt > /dev/null 2>&1
+~/GitHub/FinalProject22/cmake-build-debug/FinalProject22 < ~/GitHub/FinalProject22/tests/slide.txt > ~/GitHub/FinalProject22/output/slideout.txt
+diff ~/GitHub/FinalProject22/output/slideout.txt ~/GitHub/FinalProject22/tests/slide.output.txt > /dev/null 2>&1
 if [ $? -eq 0 ]; then
     echo "Test 'Slide' passed"
 else
@@ -31,8 +31,8 @@ else
 fi
 
 echo "Running test 1..."
-$HOME/GitHub/FinalProject22/build/FinalProject22 < $HOME/GitHub/FinalProject22/tests/test1.txt > $HOME/GitHub/FinalProject22/output/test1o.txt
-diff $HOME/GitHub/FinalProject22/output/test1o.txt $HOME/GitHub/FinalProject22/tests/test1.output.txt > /dev/null 2>&1
+~/GitHub/FinalProject22/cmake-build-debug/FinalProject22 < ~/GitHub/FinalProject22/tests/test1.txt > ~/GitHub/FinalProject22/output/test1o.txt
+diff ~/GitHub/FinalProject22/output/test1o.txt ~/GitHub/FinalProject22/tests/test1.output.txt > /dev/null 2>&1
 if [ $? -eq 0 ]; then
     echo "Test 1 passed"
 else
@@ -40,8 +40,8 @@ else
 fi
 
 echo "Running test 2..."
-$HOME/GitHub/FinalProject22/build/FinalProject22 < $HOME/GitHub/FinalProject22/tests/test2.txt > $HOME/GitHub/FinalProject22/output/test2o.txt
-diff $HOME/GitHub/FinalProject22/output/test2o.txt $HOME/GitHub/FinalProject22/tests/test2.output.txt > /dev/null 2>&1
+~/GitHub/FinalProject22/cmake-build-debug/FinalProject22 < ~/GitHub/FinalProject22/tests/test2.txt > ~/GitHub/FinalProject22/output/test2o.txt
+diff ~/GitHub/FinalProject22/output/test2o.txt ~/GitHub/FinalProject22/tests/test2.output.txt > /dev/null 2>&1
 if [ $? -eq 0 ]; then
     echo "Test 2 passed"
 else
@@ -49,8 +49,8 @@ else
 fi
 
 echo "Running test 3..."
-$HOME/GitHub/FinalProject22/build/FinalProject22 < $HOME/GitHub/FinalProject22/tests/test3.txt > $HOME/GitHub/FinalProject22/output/test3o.txt
-diff $HOME/GitHub/FinalProject22/output/test3o.txt $HOME/GitHub/FinalProject22/tests/test3.output.txt > /dev/null 2>&1
+~/GitHub/FinalProject22/cmake-build-debug/FinalProject22 < ~/GitHub/FinalProject22/tests/test3.txt > ~/GitHub/FinalProject22/output/test3o.txt
+diff ~/GitHub/FinalProject22/output/test3o.txt ~/GitHub/FinalProject22/tests/test3.output.txt > /dev/null 2>&1
 if [ $? -eq 0 ]; then
     echo "Test 3 passed"
 else
